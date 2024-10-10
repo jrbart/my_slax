9a206853b2668c26c905d651f4dc4d083d82a3a8
jrbart <jorabart@gmail.com>
2024-10-09 20:37:21 -0500

    set page title in ChatRoomLive

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -78,7 +78,7 @@ defmodule SlaxWeb.ChatRoomLive do
         :error ->
           Chat.get_first_room!()
      end
-    {:noreply, assign(socket, hide_topic?: false, room: room)}
+    {:noreply, assign(socket, hide_topic?: false, page_title: "#" <> room.name, room: room)}
   end
 
   def handle_event("toggle-topic", _params, socket) do
