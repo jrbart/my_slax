cb72dff6fb6a735c5a6a360f7d0ad91f302c5350
jrbart <jorabart@gmail.com>
2024-10-11 12:37:39 -0500

    show timestamps in correct timezone

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -87,7 +87,12 @@ defmodule SlaxWeb.ChatRoomLive do
         </ul>
       </div>
       <div id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
-       <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} />
+       <.message 
+        :for={{dom_id, message} <- @streams.messages} 
+        dom_id={dom_id} 
+        message={message}
+        timezone={@timezone}
+        />
      </div>
     <div class="h-12 bg-white px-4 pb-4">
       <.form
@@ -117,6 +122,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   attr :dom_id, :string, required: true
   attr :message, Message, required: true
+  attr :timezone, :string, required: true
 
  defp message(assigns) do
    ~H"""
@@ -127,7 +133,9 @@ defmodule SlaxWeb.ChatRoomLive do
          <.link class="text-sm font-semibold hover:underline">
            <span><%= username(@message.user) %></span>
          </.link>
-         <span class="m1-1 text-xs text-gray-500"><%= message_timestamp(@message) %></span>
+         <span :if={@timezone} class="m1-1 text-xs text-gray-500">
+           <%= message_timestamp(@message, @timezone) %>
+         </span>
          <p class="text-sm"><%= @message.body %></p>
        </div>
      </div>
@@ -135,8 +143,9 @@ defmodule SlaxWeb.ChatRoomLive do
    """
   end
 
-  defp message_timestamp(message) do
+  defp message_timestamp(message, timezone) do
     message.inserted_at
+    |> Timex.Timezone.convert(timezone)
     |> Timex.format!("%-l:%M %p", :strftime)
   end
 
@@ -167,7 +176,9 @@ defmodule SlaxWeb.ChatRoomLive do
   def mount( _params, _session, socket) do
     rooms = Chat.list_rooms()
 
-    {:ok, assign(socket, rooms: rooms)}
+    timezone = get_connect_params(socket)["timezone"]
+
+    {:ok, assign(socket, rooms: rooms, timezone: timezone)}
   end
 
   def handle_params(params, _session, socket) do
