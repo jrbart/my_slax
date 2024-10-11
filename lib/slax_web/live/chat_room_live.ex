11527ba0106f63b4b5d649fbbdf93c192dd42f08
jrbart <jorabart@gmail.com>
2024-10-11 12:15:30 -0500

    add timestamps formatted with Timex

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -127,11 +127,17 @@ defmodule SlaxWeb.ChatRoomLive do
          <.link class="text-sm font-semibold hover:underline">
            <span><%= username(@message.user) %></span>
          </.link>
+         <span class="m1-1 text-xs text-gray-500"><%= message_timestamp(@message) %></span>
          <p class="text-sm"><%= @message.body %></p>
        </div>
      </div>
    </div>
    """
+  end
+
+  defp message_timestamp(message) do
+    message.inserted_at
+    |> Timex.format!("%-l:%M %p", :strftime)
   end
 
   defp username(user) do
