315de850ba75271e72e78701a3c5941a57007a22
jrbart <jorabart@gmail.com>
2024-10-15 08:59:16 -0500

    add placeholder avatars

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -213,7 +213,7 @@ defmodule SlaxWeb.ChatRoomLive do
      >
       <.icon :if={@current_user.id == @message.user_id} name="hero-trash" class="h-4 w-4" />
      </button>
-     <div class="h-10 w-10 rounded flex-shrink-0 bg-slate-300"></div>
+     <img class="h-10 w-10 rounded flex-shrink-0" src={~p"/images/one_ring.jpg"} />
      <div class="ml-2">
        <div class="-mt-1">
          <.link class="text-sm font-semibold hover:underline">
