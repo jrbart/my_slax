afdbdeab6fc3f467c992e67fb88b75d01010a5eb
jrbart <jorabart@gmail.com>
2024-10-24 08:29:01 -0500

    add ProfileComponent

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -91,45 +91,35 @@ defmodule SlaxWeb.ChatRoomLive do
           </div>
         </div>
         <ul class="relative z-10 flex items-center gap-4 px-4 sm:px-6 lg:px-8 justify-end">
-          <%= if @current_user do %>
-            <li class="text-[0.8125rem] leading-6 text-zinc-900">
-              <%= @current_user.username %>
-            </li>
-            <li>
+          <li class="text-[0.8125rem] leading-6 text-zinc-900">
+            <div class="test-sm leading-10">
               <.link
-                href={~p"/users/settings"}
-                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+                class="flex gap-4 items-center"
+                phx-click="show-profile"
+                phx-value-user-id={@current_user.id}
               >
-                Settings
+                <img src={~p"/images/one_ring.jpg"} class="h-8 w-8 rounded" />
+                <span class="hover:underline"><%= @current_user.username %></span>
               </.link>
-            </li>
-            <li>
-              <.link
-                href={~p"/users/log_out"}
-                method="delete"
-                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
-              >
-                Log out
-              </.link>
-            </li>
-          <% else %>
-            <li>
-              <.link
-                href={~p"/users/register"}
-                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
-              >
-                Register
-              </.link>
-            </li>
-            <li>
-              <.link
-                href={~p"/users/log_in"}
-                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
-              >
-                Log in
-              </.link>
-            </li>
-          <% end %>
+            </div>
+          </li>
+          <li>
+            <.link
+              href={~p"/users/settings"}
+              class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+            >
+              Settings
+            </.link>
+          </li>
+          <li>
+            <.link
+              href={~p"/users/log_out"}
+              method="delete"
+              class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+            >
+              Log out
+            </.link>
+          </li>
         </ul>
       </div>
       <div
@@ -214,7 +204,14 @@ defmodule SlaxWeb.ChatRoomLive do
         </div>
       </div>
     </div>
-    <.modal id="new-room-modal" show={@live_action == :new} on_cancel={JS.patch(~p"/rooms/#{@room}")}>
+    <%= if assigns[:profile] do %>
+      <.live_component id="profile" module={SlaxWeb.ChatRoomLive.ProfileComponent} user={@profile} />
+    <% end %>
+    <.modal 
+      id="new-room-modal" 
+      show={@live_action == :new} 
+      on_cancel={JS.patch(~p"/rooms/#{@room}")}
+    >
       <.header>New chat room</.header>
       <.live_component
         module={SlaxWeb.ChatRoomLive.FormComponent}
@@ -287,11 +284,19 @@ defmodule SlaxWeb.ChatRoomLive do
       >
         <.icon :if={@current_user.id == @message.user_id} name="hero-trash" class="h-4 w-4" />
       </button>
-      <img class="h-10 w-10 rounded flex-shrink-0" src={~p"/images/one_ring.jpg"} />
+      <img 
+        class="h-10 w-10 rounded flex-shrink-0" 
+        phx-click="show-profile"
+        phx-value-user-id={@message.user.id}
+        src={~p"/images/one_ring.jpg"} 
+      />
       <div class="ml-2">
         <div class="-mt-1">
-          <.link class="text-sm font-semibold hover:underline">
-            <span><%= @message.user.username %></span>
+          <.link 
+            phx-click="delete-message"
+            phx-value-id={@message.id}
+            class="text-sm font-semibold hover:underline">
+            <%= @message.user.username %>
           </.link>
           <span :if={@timezone} class="m1-1 text-xs text-gray-500">
             <%= message_timestamp(@message, @timezone) %>
@@ -464,6 +469,10 @@ defmodule SlaxWeb.ChatRoomLive do
     assign(socket, :new_message_form, to_form(changeset))
   end
 
+  def handle_event("close-profile", _, socket) do
+    {:noreply, assign(socket, :profile, nil)}
+  end
+
   def handle_event("delete-message", %{"id" => id}, socket) do
     Chat.delete_message_by_id(id, socket.assigns.current_user)
 
@@ -511,6 +520,11 @@ defmodule SlaxWeb.ChatRoomLive do
       end
 
     {:noreply, socket}
+  end
+
+  def handle_event("show-profile", %{"user-id" => user_id}, socket) do
+    user =Accounts.get_user!(user_id)
+    {:noreply, assign(socket, :profile, user)}
   end
 
   def handle_info({:new_message, message}, socket) do
