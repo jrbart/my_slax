71dc8a0903cf6d62b3198017b0de24a8749497bb
jrbart <jorabart@gmail.com>
2024-10-25 07:19:33 -0500

    extract UserComponents

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -7,6 +7,8 @@ defmodule SlaxWeb.ChatRoomLive do
   alias Slax.Chat.{Message, Room}
   alias SlaxWeb.OnlineUsers
 
+  import SlaxWeb.UserComponents
+
   def render(assigns) do
     ~H"""
     <div class="flex flex-col flex-shrink-0 w-64 bg-slate-100">
@@ -98,7 +100,7 @@ defmodule SlaxWeb.ChatRoomLive do
                 phx-click="show-profile"
                 phx-value-user-id={@current_user.id}
               >
-                <img src={~p"/images/one_ring.jpg"} class="h-8 w-8 rounded" />
+                <.user_avatar user={@current_user} class="h-8 w-8 rounded" />
                 <span class="hover:underline"><%= @current_user.username %></span>
               </.link>
             </div>
@@ -284,11 +286,11 @@ defmodule SlaxWeb.ChatRoomLive do
       >
         <.icon :if={@current_user.id == @message.user_id} name="hero-trash" class="h-4 w-4" />
       </button>
-      <img 
-        class="h-10 w-10 rounded flex-shrink-0" 
+      <.user_avatar
+        user={@message.user}
+        class="h-10 w-10 rounded cursor-pointer" 
         phx-click="show-profile"
         phx-value-user-id={@message.user.id}
-        src={user_avatar_path(@message.user)} 
       />
       <div class="ml-2">
         <div class="-mt-1">
@@ -306,14 +308,6 @@ defmodule SlaxWeb.ChatRoomLive do
       </div>
     </div>
     """
-  end
-
-  defp user_avatar_path(user) do
-    if user.avatar_path do
-      ~p"/uploads/#{user.avatar_path}"
-    else
-      ~p"/images/one_ring.jpg"
-    end
   end
 
   defp message_timestamp(message, timezone) do
