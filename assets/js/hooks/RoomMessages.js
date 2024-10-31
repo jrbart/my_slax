ceb15e5e5bcdb16428d34fa403e01decf37cfad7
jrbart <jorabart@gmail.com>
2024-10-31 06:47:40 -0500

    paginate messages automatically on scroll

--- assets/js/hooks/RoomMessages.js
+++ assets/js/hooks/RoomMessages.js
@@ -11,7 +11,23 @@ const RoomMessages = {
         avatar.src = `/uploads/${avatar_path}`
       });
     });
-  }
-};
+
+    this.canLoadMore = true;
+
+    this.el.addEventListener("scroll", e => {
+      if (this.canLoadMore && this.el.scrollTop < 100) {
+        this.canLoadMore = false;
+        const prevHeight = this.el.scrollHeight;
+        this.pushEvent("load-more-messages",{}, (reply) => {
+          this.el.scrollTo(0, this.el.scrollHeight - prevHeight);
+          this.canLoadMore = reply.can_load_more;
+        });
+      }
+    });
+
+    this.handleEvent("reset_pagination", ({can_load_more}) => {
+      this.canLoadMore = can_load_more;
+    })
+  }};
 
 export default RoomMessages;
