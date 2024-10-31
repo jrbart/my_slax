dfe98fd641cb630057f773a07bbffa771b7fd9e3
jrbart <jorabart@gmail.com>
2024-10-31 07:06:23 -0500

    add Reaction schema

--- lib/slax/chat/message.ex
+++ lib/slax/chat/message.ex
@@ -3,12 +3,13 @@ defmodule Slax.Chat.Message do
   import Ecto.Changeset
 
   alias Slax.Accounts.User
-  alias Slax.Chat.{Reply,Room}
+  alias Slax.Chat.{Reaction, Reply,Room}
 
   schema "messages" do
     field :body, :string
     belongs_to :room, Room
     belongs_to :user, User
+    has_many :reactions, Reaction
     has_many :replies, Reply
 
     timestamps(type: :utc_datetime)
